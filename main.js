85bbc94ff0175080f1fe523341c7677ba4a2a89e
Scot Morse <morses@wou.edu>
2020-09-11 16:38:50 -0700

    Initial commit of plain home page and javascript file

---
+++ main.js
@@ -0,0 +1,6 @@
+function hello() {
+    let msg = document.createElement("p");
+    let txt = document.createTextNode("Hello Javascript");
+    msg.appendChild(txt);
+    document.querySelector("body").appendChild(msg);
+}
